3d447729b63da8e2bae7ff4ad6e8f815ecbfc9b9
t0ri <victoriarosemurray@gmail.com>
2020-07-14 15:02:36 -0700

    convert to class component and add state

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from './Routes/Routes';
 
+import AppBar from './Components/AppBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Routes />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: 'user',
+    };
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return (
+      <div className="App">
+        <Router>
+          <AppBar />
+          <Routes user={user} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
