045feb41d736cfa2836d84a818632e81c0a13617
t0ri <victoriarosemurray@gmail.com>
2020-07-14 14:46:48 -0700

    update routes

--- client/src/Routes/Routes.js
+++ client/src/Routes/Routes.js
@@ -1,43 +1,39 @@
 /* eslint-disable no-unused-expressions */
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 // Routes
 import Home from './Home/Home';
 import ShowProjects from './ShowProjects/ShowProjects';
 import ShowProject from './ShowProject/ShowProject';
-import NewProject from './NewProject/NewProject';
 
 export default function Routes({ user, project }) {
   return (
-    <Router>
+    <div>
       {/* "/" => ShowProjects */}
       <Route
         exact
         path="/"
-        render={() => {
+        render={() => (
           user ? (
             <ShowProjects user={user} />
           ) : (
             <Home />
-          );
-        }}
+          )
+        )}
       />
 
       {/* "/project/:id" => ShowProject */}
       <Route
-        path="/project/:projectId"
-        render={() => {
+        path="/project/"
+        render={() => (
           user ? (
             <ShowProject user={user} project={project} />
           ) : (
             <Home />
-          );
-        }}
+          )
+        )}
       />
-    </Router>
+    </div>
   );
 }
