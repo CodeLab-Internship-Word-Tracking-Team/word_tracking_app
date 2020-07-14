0c44fc8cac13d0933e86ff93943148a25d4972ab
t0ri <victoriarosemurray@gmail.com>
2020-07-14 15:01:12 -0700

    unify project pages

--- client/src/Routes/ShowProject/ShowProject.js
+++ client/src/Routes/ShowProject/ShowProject.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ShowProject() {
   return (
     <div>
-      show project page renders when a page is clicked on
+      <h1>show project page renders when a page is clicked on</h1>
     </div>
   );
 }
