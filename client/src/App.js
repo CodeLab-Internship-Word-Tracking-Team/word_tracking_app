5347e4e70fac294c86491aa78189d7ed3a606b32
t0ri <victoriarosemurray@gmail.com>
2020-07-13 19:51:01 -0700

    init router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Routes from './Routes/Routes';
+
 import AppBar from './Components/AppBar';
-import Projects from './Components/Projects';
 import './App.css';
-import './Components/AppBar.js';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <AppBar />
-        <Route path="/projects" component={Projects} />
+        <Routes />
       </div>
     </Router>
   );
---
+++ client/src/Routes/Routes.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+// Routes
+import Home from './Home';
+import ShowProjects from './ShowProjects/ShowProjects';
+import ShowProject from './ShowProject/ShowProject';
+import NewProject from './NewProject/NewProject';
+
+export default function Routes({ user, project }) {
+  return (
+    <Router>
+      {/* "/" => ShowProjects */}
+      <Route
+        exact
+        path="/"
+        render={() => {
+          user ? (
+            <ShowProjects user={user} />
+          ) : (
+            <Home />
+          );
+        }}
+      />
+
+      {/* "/project/:id" => ShowProject */}
+      <Route
+        path="/project/:projectId"
+        render={() => {
+          user ? (
+            <ShowProject user={user} project={project} />
+          ) : (
+            <Home />
+          );
+        }}
+      />
+
+      {/* "/project/new" => NewProject */}
+      <Route
+        path="/project/new"
+        render={() => {
+          user ? (
+            <NewProject user={user} />
+          ) : (
+            <Home />
+          );
+        }}
+      />
+    </Router>
+  );
+}
