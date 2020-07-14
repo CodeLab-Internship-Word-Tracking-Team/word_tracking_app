e6408104e3332f3f5c0350b81f8ab0576b9e92ca
t0ri <victoriarosemurray@gmail.com>
2020-07-14 14:11:00 -0700

    update example pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 
 import Routes from './Routes/Routes';
 
-import AppBar from './Components/AppBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <AppBar />
       <Routes />
     </div>
   );
---
+++ client/src/Routes/ShowProjects/ShowProjects.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ShowProjects extends Component {
+  render() {
+    return (
+      <div>
+        show projects
+      </div>
+    )
+  }
+}
