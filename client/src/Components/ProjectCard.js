7dd7bde21c94c0479216dca1068d6426d03f788c
Melanie Locke <melanie.a.locke@gmail.com>
2020-07-13 22:51:47 -0600

    Add project view button

--- client/src/Components/ProjectCard.js
+++ client/src/Components/ProjectCard.js
@@ -1,13 +1,18 @@
 import React from "react";
 import '../Components/ProjectCard.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 function ProjectCard() {
+
     return (
         <div className="project-card-container">
             <div className="project-card-content">
                 <h2>Project Title</h2>
                 <p>Project description or tagline. We'll need to decide at what point the words get cut...</p>
-                <progress id="progress-bar" className="project-card-progress-bar" value="20" max="100"></progress><label for="progress-bar" className="project-card-progress-label">20%</label>
+                <progress id="progress-bar" className="project-card-progress-bar" value="20" max="100"></progress>
+                <label for="progress-bar" className="project-card-progress-label">20%</label>
+                <Button variant="outlined" className="project-card-view-btn">VIEW PROJECT</Button>
             </div>
         </div>
     );
