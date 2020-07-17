5d40d1f2da666ec6035091a546871a2b3df37a80
t0ri <victoriarosemurray@gmail.com>
2020-07-17 13:22:39 -0700

    state wired

--- client/src/Routes/ShowProject/ShowProject.js
+++ client/src/Routes/ShowProject/ShowProject.js
@@ -8,24 +8,19 @@ export default function ShowProject({ user, projectId }) {
   const [project, setProject] = useState([]);
   const fetchProject = async () => {
     // change `/projects/` to `/project/:projectId` once route exists
-    const data = await fetch('/projects');
+    const response = await fetch('/projects');
     // change to `project`
-    const projects = await data.json();
+    const data = await response.json();
     // change `projects.projects[0]` to just `project`
-    const pro = Object.values(projects.projects);
-    const progress = Object.values(pro[0]);
-    console.log(progress[3]['wordCount']);
-    // console.dir(pro[0].id);
-    setProject(Object.values(projects.projects));
+    setProject(data.projects[0]);
   };
-
   return (
     <div>
-      {/* <div key={project[0].id}> */}
-        {/* <h1>{project.projects[0].name}</h1> */}
-        {/* <h1>{project.description}</h1> */}
-        {/* <h1>{project['progress']['wordGoal']}</h1> */}
-      {/* </div> */}
+      <div key={project.id}>
+        <h1>{project.name}</h1>
+        <h1>{project.description}</h1>
+        {/* <h1>{project['progress']['word goal']}</h1> */}
+      </div>
     </div>
   );
 }
