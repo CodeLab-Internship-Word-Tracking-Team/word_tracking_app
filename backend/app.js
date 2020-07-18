603226d797793271410bc0af2ec18748aa10db3e
Jason Drew Panzer <jason.drew.panzer@gmail.com>
2020-07-17 22:06:37 -0700

    Sets up npm project and package file in backend creates scripts file adds nodemon to startup script to make development simpler install mongoose package for connection to mongodb adds skeleton connection to mongoose skeleton of db initialization script

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var app = express();
 
+var mongoose = require('mongoose');
+
+/* 
+   The following section is pulled from 
+   https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
+   and is an example of making a db connection using mongoose.
+   You'll need your Atlas credentials for a real version, and
+   your user name and password should be stored in a separate
+   file that is not committed to the repo. More information
+   can be found at https://mongoosejs.com/docs/connections.html
+   - json
+*/
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+/* End DB section */
+
 app.use(function(req, res, next) {
    // Website you wish to allow to connect
    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
@@ -22,7 +47,6 @@ app.use(function(req, res, next) {
 app.get('/', function(req, res){
    res.json({ user: 'tobi' });
 });
-
 
 app.get('/projects', function(req, res){
    res.json(
---
+++ backend/scripts/dbdeinit.js
@@ -0,0 +1,4 @@
+/*
+    Think about the inverse of the dbinit.js operation...
+    Hint: https://mongoosejs.com/docs/models.html#deleting
+*/
