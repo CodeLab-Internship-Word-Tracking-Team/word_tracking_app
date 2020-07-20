67d1748ce72a32d498e7d2c2f0629ffee5e11296
t0ri <victoriarosemurray@gmail.com>
2020-07-20 16:32:13 -0700

    add description component and styles

--- client/src/Routes/ShowProject/Components/ProjectDescription/ProjectDescription.js
+++ client/src/Routes/ShowProject/Components/ProjectDescription/ProjectDescription.js
@@ -2,25 +2,28 @@
 import React from 'react';
 
 // Material UI Imports
+import { Typography, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Container } from '@material-ui/core';
 
 const useStyles = makeStyles({
-  container: {
-    maxWidth: '40%',
+  author: {
+    paddingBottom: '1vh',
+    color: '#424242',
+  },
+  description: {
+    paddingBottom: '2vh',
   },
 });
 
 export default function ProjectDescription({ project }) {
   const { name, author, description } = project;
-
   const classes = useStyles();
 
   return (
-    <Container className={classes.container} disableGutters>
-      <Typography variant="h3">{name}</Typography>
-      <Typography variant="h4">{author}</Typography>
-      <Typography variant="body1">{description}</Typography>
-    </Container>
+    <Grid disableGutters justify="space-between">
+      <Typography variant="h3" className={classes.title}>{name}</Typography>
+      <Typography variant="h4" className={classes.author}>{`by ${author}`}</Typography>
+      <Typography variant="body1" className={classes.description}>{description}</Typography>
+    </Grid>
   );
 }
