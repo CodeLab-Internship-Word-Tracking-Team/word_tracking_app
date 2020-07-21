9ff0e481b4b3818fb489de14f63a54eedafdf6da
t0ri <victoriarosemurray@gmail.com>
2020-07-21 14:58:49 -0700

    lint code

--- client/src/Components/ProjectCard.js
+++ client/src/Components/ProjectCard.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 // Styling
-import '../Components/ProjectCard.css';
+import './ProjectCard.css';
 
 // Material UI
 import { makeStyles } from '@material-ui/core/styles';
@@ -14,43 +14,41 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-    title: {
-        fontSize: 28,
-    },
+  title: {
+    fontSize: 28,
+  },
 });
 
 export default function ProjectCard() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    useEffect(() => {
-        fetchItems();
-      }, []);
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    const fetchItems = async () => {
-    const data = await fetch('http://localhost:3000/projects');
+  const fetchItems = async () => {
+    const response = await fetch('http://localhost:3000/projects');
+    const data = await response.json();
+    setItems(data.projects);
+  };
 
-    let items = await data.json();
-    items = items["projects"];
-    setItems(items);
-    }
-
-    return (
-        <div>
-            <Card className={classes.root} variant="outlined">
-                <CardContent>
-                    <Typography className={classes.title}>Project Name</Typography>
-                    <p>Here is a description of a project. It will have a lot of words...</p>
-                    <progress id="progress-bar" className="project-card-progress-bar" value="20" max="100"></progress>
-                    <label htmlFor="progress-bar" className="project-card-progress-label">20%</label>
-                    <CardActions>
-                        <Link to="/project">
-                            <Button variant="outlined">VIEW PROJECT</Button>
-                        </Link>
-                    </CardActions>
-                </CardContent>
-            </Card>
-        </div>
-    );
+  return (
+    <div>
+      <Card className={classes.root} variant="outlined">
+        <CardContent>
+          <Typography className={classes.title}>Project Name</Typography>
+          <p>Here is a description of a project. It will have a lot of words...</p>
+          <progress id="progress-bar" className="project-card-progress-bar" value="20" max="100" />
+          <label htmlFor="progress-bar" className="project-card-progress-label">20%</label>
+          <CardActions>
+            <Link to="/project">
+              <Button variant="outlined">VIEW PROJECT</Button>
+            </Link>
+          </CardActions>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
--- client/src/Components/ProjectGrid.js
+++ client/src/Components/ProjectGrid.js
@@ -1,35 +1,33 @@
-// React
+// React Imports
 import React from 'react';
 
-// Material UI
+// Material UI Imports
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+
+// Component Imports
 import ProjectCard from './ProjectCard';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(1),
-        textAlign: 'center',
-    },
-}));
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
 
 export default function ProjectGrid() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const cards = [1, 2, 3, 4, 5];
+  const cards = [1, 2, 3, 4, 5];
 
-    return (
-        <div className={classes.root}>
-            <Grid container spacing={3}>
-                {cards.map(card => (
-                    <Grid item xs={12} lg={4} key={card.toString()}>
-                        <ProjectCard />
-                    </Grid>
-                ))}
-            </Grid>
-        </div>
-    )
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        {cards.map((card) => (
+          <Grid item xs={12} lg={4} key={card.toString()}>
+            <ProjectCard />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 }
