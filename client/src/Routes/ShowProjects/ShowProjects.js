fa36f7d7cd1752a587839352e2e2eb4262365b9f
t0ri <victoriarosemurray@gmail.com>
2020-07-21 15:42:34 -0700

    add focusProject prop to lift projectId

--- client/src/Routes/ShowProjects/ShowProjects.js
+++ client/src/Routes/ShowProjects/ShowProjects.js
@@ -1,31 +1,33 @@
-// React
+// React Imports
 import React from 'react';
 
 // Material UI imports
 import { makeStyles } from '@material-ui/core/styles';
-import ProjectGrid from '../../Components/ProjectGrid';
 import { Button, Grid } from '@material-ui/core';
+
+// Component Imports
+import ProjectGrid from '../../Components/ProjectGrid';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
     },
-  }, 
+  },
 }));
 
-export default function ShowProjects() {
+export default function ShowProjects({ focusProject }) {
   return (
     <div>
       <h1>Your Projects</h1>
-      <ProjectGrid />
+      <ProjectGrid focusProject={focusProject} />
 
       {/* Btn for add new project/ */}
       <Grid container alignItems="flex-start" justify="flex-end" direction="row">
         <Button variant="contained" color="primary">
           +
         </Button>
-      </Grid>  
+      </Grid>
     </div>
   );
 }
