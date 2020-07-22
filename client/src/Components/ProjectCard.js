063731b28034035daa84ecce831fe1fddf60bfe3
t0ri <victoriarosemurray@gmail.com>
2020-07-22 14:44:25 -0700

    add content to card

--- client/src/Components/ProjectCard.js
+++ client/src/Components/ProjectCard.js
@@ -1,17 +1,15 @@
-// React imports
-import React, { useState, useEffect } from 'react';
+// React Imports
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-// Styling
+// Styling Imports
 import './ProjectCard.css';
 
-// Material UI
+// Material UI Imports
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import {
+  Card, CardActions, CardContent, Button, Typography,
+} from '@material-ui/core';
 
 const useStyles = makeStyles({
   title: {
@@ -19,34 +17,28 @@ const useStyles = makeStyles({
   },
 });
 
-export default function ProjectCard({ focusProject }) {
+export default function ProjectCard({ project, focusProject }) {
+  const {
+    name, description, _id: id, word_count: wordCount, word_goal: wordGoal,
+  } = project;
+
   const classes = useStyles();
 
-  useEffect(() => {
-    fetchItems();
-  }, []);
-
-  const [items, setItems] = useState([]);
-
-  const fetchItems = async () => {
-    const response = await fetch('http://localhost:3000/projects');
-    const data = await response.json();
-    setItems(data.projects);
-  };
-
   const handleClick = () => {
-    // Replace string with project._id when available
-    focusProject('5f162f0bbe5fa9a20ed8e33b');
+    focusProject(id);
   };
 
   return (
     <div>
       <Card className={classes.root} variant="outlined">
         <CardContent>
-          <Typography className={classes.title}>Project Name</Typography>
-          <p>Here is a description of a project. It will have a lot of words...</p>
-          <progress id="progress-bar" className="project-card-progress-bar" value="20" max="100" />
-          <label htmlFor="progress-bar" className="project-card-progress-label">20%</label>
+          <Typography className={classes.title}>{name}</Typography>
+          <Typography variant="body1" noWrap>{description}</Typography>
+          <progress id="progress-bar" className="project-card-progress-bar" value={wordCount} max={wordGoal} />
+          <label htmlFor="progress-bar" className="project-card-progress-label">
+            {Math.round((wordCount / wordGoal) * 100)}
+            %
+          </label>
           <CardActions>
             <Link to="/project">
               <Button variant="outlined" onClick={handleClick}>VIEW PROJECT</Button>
