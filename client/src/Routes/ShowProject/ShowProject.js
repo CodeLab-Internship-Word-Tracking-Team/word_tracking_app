0ac0f9353a34fedc89ac6e612bf0b3bfb65e447c
t0ri <victoriarosemurray@gmail.com>
2020-07-24 16:27:12 -0700

    add modal control

--- client/src/Routes/ShowProject/ShowProject.js
+++ client/src/Routes/ShowProject/ShowProject.js
@@ -3,36 +3,29 @@ import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 
 // Material UI Imports
-import { Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Container, Button } from '@material-ui/core';
 
 // Component Imports
 import ProjectDescription from './Components/ProjectDescription/ProjectDescription';
 import ProjectStatistics from './Components/ProjectStatistics/ProjectStatistics';
+import ProjectEdit from './Components/ProjectEdit/ProjectEdit';
 
-const useStyles = makeStyles({
-  // container: {
-  //   maxWidth: '50vw',
-  // },
-  // statistics: {
-  //   maxWidth: '45vw',
-  // },
-});
-
-export default function ShowProject({ user, projectId }) {
+export default function ShowProject({ projectId }) {
+  // GET Project from `projectId`
   const [project, setProject] = useState([]);
   const fetchProject = async () => {
     const response = await fetch(`http://localhost:3000/project/${projectId}`);
     const data = await response.json();
     setProject(data[0]);
   };
+  useEffect(() => { fetchProject(); }, []);
 
-  useEffect(() => {
-    fetchProject();
-  }, []);
+  // Modal Control
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const handleOpen = () => { setModalOpen(true); };
+  const handleClose = () => { setModalOpen(false); };
 
-  const classes = useStyles();
-
+  // Change route if page is opened without passing `projectId`
   if (projectId === undefined) {
     return (
       <Redirect from="/project" to="/" />
@@ -40,11 +33,13 @@ export default function ShowProject({ user, projectId }) {
   }
 
   return (
-    <Container className={classes.container} disableGutters maxWidth="md">
+    <Container disableGutters maxWidth="md">
       <ProjectDescription project={project} />
-      <Container className={classes.statistics}>
+      <Container>
         <ProjectStatistics project={project} />
       </Container>
+      <Button onClick={handleOpen}>EDIT PROJECT</Button>
+      <ProjectEdit open={modalOpen} onClose={handleClose} project={project} />
     </Container>
   );
 }
