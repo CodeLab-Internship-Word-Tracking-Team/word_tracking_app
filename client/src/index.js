5dc44a21bd8c4638c9156838ac15f11bf56c9911
t0ri <victoriarosemurray@gmail.com>
2020-07-29 17:07:03 -0700

    add wrapper

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="dev-word-tracking-app.us.auth0.com"
+    clientId="rFbv17C82s0HFO7wiuc6HXKyReuGS9dP"
+    redirectUri={window.location.origin}
+    audience="https://dev-word-tracking-app.us.auth0.com/api/v2/"
+    scope="read:current_user update:current_user_metadata"
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
   document.getElementById('root'),
 );
 
