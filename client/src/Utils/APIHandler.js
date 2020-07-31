e6748cedfc9efa967e858d6a02b3e09be6ba4be7
t0ri <victoriarosemurray@gmail.com>
2020-07-30 17:50:47 -0700

    use token in requestConfig

--- client/src/Utils/APIHandler.js
+++ client/src/Utils/APIHandler.js
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
 // Set Headers for Requests
-const requestConfig = {
+const requestConfig = (token) => ({
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_BEARER_TOKEN}`,
+    Authorization: `Bearer ${token}`,
     'Content-Type': 'application/json',
   },
-};
+});
 
 export default {
   // GET all projects -- await API.getProjects()
-  getProjects: () => axios.get('/projects', requestConfig),
+  getProjects: (token) => axios.get('/projects', requestConfig(token)),
 
   // GET 1 project -- await API.getProject(projectId)
-  getProject: (projectId) => axios.get(`/project/${projectId}`, requestConfig),
+  getProject: (token, projectId) => axios.get(`/project/${projectId}`, requestConfig(token)),
 
   // POST project -- await API.newProject({ ... })
-  newProject: (projectData) => axios.post('/project', projectData, requestConfig),
+  newProject: (token, projectData) => axios.post('/project', projectData, requestConfig(token)),
 
   // PUT 1 project -- await API.newProject(projectId, { ... })
-  updateProject: (projectId, projectData) => axios.put(`/project/${projectId}`, projectData, requestConfig),
+  updateProject: (token, projectId, projectData) => axios.put(`/project/${projectId}`, projectData, requestConfig(token)),
 
   // DELETE 1 project -- await API.deleteProject(projectId)
-  deleteProject: (projectId) => axios.delete(`/project/${projectId}`, requestConfig),
+  deleteProject: (token, projectId) => axios.delete(`/project/${projectId}`, requestConfig(token)),
 };
