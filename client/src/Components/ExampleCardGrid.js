76a0177c3213b92d37326ac4010108196a2e8597
Melanie Locke <melanie.a.locke@gmail.com>
2020-07-30 18:24:29 -0600

    Add unauthorized login homepage

---
+++ client/src/Components/ExampleCardGrid.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+// Material UI Imports
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Grid, Card, CardContent, Typography,
+} from '@material-ui/core';
+
+// Styling Imports
+import './ProjectCard.css';
+
+const useStyles = makeStyles({
+    root: {
+        minHeight: "25vh",
+    },
+    title: {
+      fontSize: 28,
+      margin: "2vh",
+    },
+    description: {
+        margin: "2vh",
+    },
+    exampleCardGridContainer: {
+        marginTop: "5vh",
+    }
+  });
+
+export default function ExampleCardGrid() {
+    const classes = useStyles();
+
+    return (
+        <Grid className={classes.exampleCardGridContainer} container spacing={3}>
+            <Grid item xs={12} lg={4}>
+                <div>
+                    <Card className={classes.root} variant="outlined">
+                        <CardContent>
+                        <Typography className={classes.title}>The Peach Princess</Typography>
+                        <Typography variant="body1" className={classes.description}>
+                            A princess finds a magic peach and becomes a fruit witch.   
+                        </Typography>
+                        <progress id="progress-bar" className="project-card-progress-bar" value="204" max="10000" />
+                        <label htmlFor="progress-bar" className="project-card-progress-label">
+                            2%
+                        </label>
+                        </CardContent>
+                    </Card>
+                </div>
+            </Grid>
+            <Grid item xs={12} lg={4}>
+                <div>
+                    <Card className={classes.root} variant="outlined">
+                        <CardContent>
+                        <Typography className={classes.title}>The Dark Knight</Typography>
+                        <Typography variant="body1" className={classes.description}>
+                            A knight who loves the night goes on a quest to save the sun.    
+                        </Typography>
+                        <progress id="progress-bar" className="project-card-progress-bar" value="21000" max="30000" />
+                        <label htmlFor="progress-bar" className="project-card-progress-label">
+                            70%
+                        </label>
+                        </CardContent>
+                    </Card>
+                </div>
+            </Grid>
+            <Grid item xs={12} lg={4}>             
+                <div>
+                    <Card className={classes.root} variant="outlined">
+                        <CardContent>
+                        <Typography className={classes.title}>Smell You Later</Typography>
+                        <Typography variant="body1" className={classes.description}>
+                            A lonely trashcan befriends chaotic creatures of the night — raccoons.    
+                        </Typography>
+                        <progress id="progress-bar" className="project-card-progress-bar" value="1091" max="5000"/>
+                        <label htmlFor="progress-bar" className="project-card-progress-label">
+                            21%
+                        </label>
+                        </CardContent>
+                    </Card>
+                </div>
+            </Grid>
+        </Grid>
+    );
+}
--- client/src/Routes/Home/Home.js
+++ client/src/Routes/Home/Home.js
@@ -1,9 +1,51 @@
 import React from 'react';
 
+// Material UI imports
+import { Grid, Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ExampleCardGrid from '../../Components/ExampleCardGrid';
+
+const useStyles = makeStyles({
+  welcomeTitle: {
+    marginTop: "15vh",
+    textAlign: "center",
+    fontSize: 36,
+  },
+  welcomeTagline: {
+    textAlign: "center",
+    fontSize: 28,
+    marginTop: '3vh',
+  },
+  welcomeButtonsContainer: {
+    textAlign: "center",
+    marginTop: "10vh",
+  },
+  welcomeButton: {
+    fullWidth: "false",
+    marginTop: "2vh",
+    marginBottom: "2vh",
+  }
+})
+
 export default function Home() {
+  const classes = useStyles();
+  
   return (
-    <div>
-      <h1>home page rendered when no user in App.js state</h1>
+    <div className={classes.welcomeContainer}>
+      <Typography className={classes.welcomeTitle} variant="h1">Welcome to Count the Words</Typography>
+      <Typography className={classes.welcomeTagline} variant="h2">
+        Keep track of all your writing projects and track your word count goals!
+      </Typography>
+      <div className={classes.welcomeButtonsContainer}>
+        <Button variant="outlined" color="primary" className={classes.welcomeButton}>
+          Create an Account
+        </Button>
+        <Typography>or</Typography>
+        <Button variant="outlined" color="primary" className={classes.welcomeButton}>
+          Log In
+        </Button>
+      </div>
+      <ExampleCardGrid />
     </div>
   );
 }
