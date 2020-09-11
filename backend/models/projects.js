766bfcb442423bf5c42aec91d59511650268d773
t0ri <victoriarosemurray@gmail.com>
2020-09-11 14:17:05 -0700

    remove events array for compatibility

--- backend/models/projects.js
+++ backend/models/projects.js
@@ -4,7 +4,7 @@ const ProjectSchema = new mongoose.Schema({
   title: { type: String, required: true, maxlength: 100 },
   description: { type: String, required: true, maxlength: 280 },
   wordGoal: { type: Number },
-  wordCountEvents: { type: Array, required: false }
+  wordCount: { type: Number }
 });
 
 module.exports = mongoose.model('Project', ProjectSchema, 'Projects');
