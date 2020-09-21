ac27ef600b9311ff89db98252d5ec7422da3f90a
t0ri <victoriarosemurray@gmail.com>
2020-09-21 16:28:48 -0700

    cleanup App clutter

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 // React Imports
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 // Auth0 Import
-import { withAuth0, useAuth0 } from '@auth0/auth0-react';
+import { withAuth0 } from '@auth0/auth0-react';
 
 // Material UI Imports
-import { Container, makeStyles } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 
 // Components & Page Imports
 import AppBar from './Components/AppBar/AppBar';
@@ -15,23 +15,12 @@ import Routes from './Routes/Routes';
 // Style Import
 import './App.scss';
 
-const useStyles = makeStyles({
-  container: {
-    maxWidth: '87.5vw',
-    margin: '2vh 6.25vw 0 6.25vw',
-    padding: '0',
-  },
-});
-
 function App() {
-  // Material UI
-  const classes = useStyles();
-
   return (
     <div className="App">
       <Router>
         <AppBar />
-        <Container className={classes.container}>
+        <Container className="container">
           <Routes />
         </Container>
       </Router>
